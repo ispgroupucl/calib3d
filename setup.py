5a396ac9949169065d46b8ec87ba446e143ba57f
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2020-09-03 14:42:47 +0000

    v1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     licence="LGPL",
     python_requires='>=3.6',
     description="Python 3D calibration and homogenous coordinates computation library",
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=["numpy", "opencv-python"],
 )
