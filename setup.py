e6dd401d9e73223abb58b1f2bfaa6b47adf29f18
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2020-09-03 10:01:49 +0000

    setup for installing lib

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calib3d',
+    author='Gabriel Van Zandycke',
+    author_email="gabriel.vanzandycke@hotmail.com",
+    url="https://github.com/ispgroupucl/calib3d",
+    licence="LGPL",
+    python_requires='>=3.6',
+    description="Python 3D calibration and homogenous coordinates computation library",
+    version='1.0',
+    packages=find_packages(),
+    install_requires=["numpy", "opencv-python"],
+)
