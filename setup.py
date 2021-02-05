07ad370d824749ae92d0ef98484833cdf9977083
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-02-05 16:20:27 +0000

    increase versino number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     licence="LGPL",
     python_requires='>=3.6',
     description="Python 3D calibration and homogenous coordinates computation library",
-    version='2.1.3',
+    version='2.1.4',
     packages=find_packages(),
     install_requires=[
         "numpy",
