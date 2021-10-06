09d3493126830bb206221ed6beca1331acda0b2d
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-10-06 09:53:47 +0000

    increase version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='2.2.1',
+    version='2.2.2',
     name='calib3d',
     author='Gabriel Van Zandycke',
     author_email="gabriel.vanzandycke@hotmail.com",
