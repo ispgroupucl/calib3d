f9a043af944cc497c5d5b59989ed4d4ffdaccb21
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-12-15 11:21:24 +0100

    add compute_shear_rectification function

--- calib3d/__init__.py
+++ calib3d/__init__.py
@@ -1,5 +1,5 @@
 __doc__ = r"""
-This library offers several tools for manipulation of calibrated cameras, projective geometry and computations using homogenous coordinates. 
+This library offers several tools for manipulation of calibrated cameras, projective geometry and computations using homogenous coordinates.
 
 Camera calibration allows to determine the relation between the camera's pixels (2D coordinates) and points in the real world
 (3D coordinates). It implies computation using homogenous coordinates. This python library aims at simplifying implementations
@@ -14,5 +14,5 @@ The different modules are document here:
 """
 
 from .points import Point3D, Point2D, VanishingPoint
-from .calib import Calib, parameters_to_affine_transform, compute_rotation_matrix, line_plane_intersection
+from .calib import Calib, parameters_to_affine_transform, compute_rotation_matrix, line_plane_intersection, compute_shear_rectification_matrix
 from .draw import ProjectiveDrawer
